d335853e26592e186f1d841071951c56caec7de8
Jose Luis Salazar <lsalazar@gmx.de>
2025-07-03 13:08:22 -0600

    Customize styles and templates

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,5 +1,6 @@
 import '@vendor/sylius/mollie-plugin/assets/shop/entrypoint';
 import './bootstrap.js';
+import './styles/custom.scss';
 
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
